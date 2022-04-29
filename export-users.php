27654e11d88d632bbc668b161e8dff0eecaa8410
Sergey Tavakalyan <sergey.t@zlogic.io>
2022-04-30 00:57:57 +0400

    minified code

--- export-users.php
+++ export-users.php
@@ -9,175 +9,157 @@ License: GPLv2 or later
 Text Domain: export-users
 */
 
-if (!defined('ABSPATH')) {
-    die;
+if ( ! defined( 'ABSPATH' ) ) {
+	die;
 }
 
-class ExportUsers
-{
+class ExportUsers {
 
-    public function register()
-    {
+	public function register() {
 
-        //add export hook
-        add_action('admin_init', [$this, 'export_all_users_csv']);
+		//add export hook
+		add_action( 'admin_init', [ $this, 'export_all_users_csv' ] );
 
-        //add button hook
-        add_action('admin_footer-users.php', array(&$this, 'bulk_admin_footer'));
-        add_action('load-users.php', array(&$this, 'bulk_action'));
-    }
+		//add button hook
+		add_action( 'admin_footer-users.php', [ &$this, 'bulk_admin_footer' ] );
+		add_action( 'load-users.php', [ &$this, 'bulk_action' ] );
+	}
 
-    public static function activation()
-    {
+	public static function activation() {
 
-        //update rewrite rules
-        flush_rewrite_rules();
-    }
+		//update rewrite rules
+		flush_rewrite_rules();
+	}
 
-    public static function deactivation()
-    {
+	public static function deactivation() {
 
-        //update rewrite rules
-        flush_rewrite_rules();
-    }
+		//update rewrite rules
+		flush_rewrite_rules();
+	}
 
-    public function bulk_admin_footer()
-    {
-        // check if the user page is
-        $screen = get_current_screen();
-        if ($screen->id != "users") {
-            return;
-        }
-        ?>
+	public function bulk_admin_footer() {
+		// check if the user page is
+		$screen = get_current_screen();
+		if ( $screen->id != "users" ) {
+			return;
+		}
+		?>
         <script>
             jQuery(document).ready(function ($) {
                 // add "Export" option
-                $('<option>').val('export').text('<?php _e('Export')?>').appendTo("select[name='action']");
+                $('<option>').val('export').text('<?php _e( 'Export' )?>').appendTo("select[name='action']");
                 // add "Export all" button
-                $('.tablenav.top .clear, .tablenav.bottom .clear').before('<form action="#" method="POST"><input type="hidden" id="export_csv" name="export-users" value="1" /><input class="button button-primary user_export_button" type="submit" value="<?php esc_attr_e('Export all', 'export-users');?>" /></form>');
+                $('.tablenav.top .clear, .tablenav.bottom .clear').before('<form action="#" method="POST"><input type="hidden" id="export_csv" name="export-users" value="1" /><input class="button button-primary user_export_button" type="submit" value="<?php esc_attr_e( 'Export all', 'export-users' );?>" /></form>');
             });
         </script>
-        <?php
-    }
+		<?php
+	}
 
-	public function bulk_action()
-    {
-        // get the action
-        $wp_list_table = _get_list_table('WP_Users_List_Table');
-        $action = $wp_list_table->current_action();
+	public function bulk_action() {
+		// get the action
+		$wp_list_table = _get_list_table( 'WP_Users_List_Table' );
+		$action        = $wp_list_table->current_action();
 
-        $allowed_actions = array("export");
-        if (!in_array($action, $allowed_actions)) {
-            return;
-        }
+		$allowed_actions = [ 'export' ];
+		if ( ! in_array( $action, $allowed_actions ) ) {
+			return;
+		}
 
-        // security check
-        check_admin_referer('bulk-users');
+		// security check
+		check_admin_referer( 'bulk-users' );
 
-        // make sure ids are submitted.
-        if (isset($_REQUEST['users'])) {
-            $user_ids = array_map('intval', $_REQUEST['users']);
-        }
+		// make sure ids are submitted.
+		if ( isset( $_REQUEST['users'] ) ) {
+			$user_ids = array_map( 'intval', $_REQUEST['users'] );
+		}
 
-        if (empty($user_ids)) {
-	        wp_safe_redirect('users.php');
-        }
+		if ( empty( $user_ids ) ) {
+			wp_safe_redirect( 'users.php' );
+		}
 
-        switch ($action) {
-            case 'export':
-                if (!$this->selected_export($user_ids)) {
-                    wp_die(__('Error exporting user.'));
-                }
-                break;
-            default:
-                return;
-        }
-        exit();
-    }
+		switch ( $action ) {
+			case 'export':
+				if ( ! $this->selected_export( $user_ids ) ) {
+					wp_die( __( 'Error exporting user.' ) );
+				}
+				break;
+			default:
+				return;
+		}
+		exit();
+	}
 
-    public function export_all_users_csv()
-    {
-        if (!empty($_POST['export-users'])) {
-            if (current_user_can('manage_options')) {
+	public function export_all_users_csv() {
+		if ( ! empty( $_POST['export-users'] ) ) {
+			if ( current_user_can( 'manage_options' ) ) {
 
-	            // the user query
-                $args = array(
-                    'order' => 'ASC',
-                    'orderby' => 'display_name',
-                    'fields' => 'all',
-                );
+				// get all users
+				$exportusers = get_users( [ 'order' => 'ASC', 'orderby' => 'display_name', 'fields' => 'all' ] );
+				$delimiter   = ",";
+				$filename    = "users-" . date( 'd-m-Y' ) . ".csv";
+				// create a file pointer
+				$out = fopen( 'php://output', 'w' );
+				// set column headers
+				$fields = [ 'Имя', 'Фамилия', 'Email', 'Роль', 'Дата регистрации' ];
 
-                // get all users
-                $exportusers = get_users($args);
+				fputcsv( $out, $fields, $delimiter );
 
-                $delimiter = ",";
-                $filename = "users-" . date('d-m-Y') . ".csv";
+				// output each row of the data, format line as csv and write to file pointer
+				foreach ( $exportusers as $user ) {
+					$meta            = get_user_meta( $user->ID );
+					$first_name      = ( isset( $meta['first_name'][0] ) && $meta['first_name'][0] != '' ) ? $meta['first_name'][0] : '';
+					$last_name       = ( isset( $meta['last_name'][0] ) && $meta['last_name'][0] != '' ) ? $meta['last_name'][0] : '';
+					$email           = $user->user_email;
+					$role            = $user->roles[0];
+					$registered_date = $user->user_registered;
+					$lineData        = [ $first_name, $last_name, $email, $role, $registered_date ];
 
-                // create a file pointer
-                $out = fopen('php://output', 'w');
+					fputcsv( $out, $lineData, $delimiter );
+				}
 
-                // set column headers
-                $fields = array('Имя', 'Фамилия', 'Email', 'Роль', 'Дата регистрации');
-                fputcsv($out, $fields, $delimiter);
+				header( "Content-type: application/force-download" );
+				header( 'Content-Disposition: inline; filename="' . $filename . '";' );
 
-                // output each row of the data, format line as csv and write to file pointer
-                foreach ($exportusers as $user) {
-                    $meta = get_user_meta($user->ID);
-	                $first_name = (isset($meta['first_name'][0]) && $meta['first_name'][0] != '') ? $meta['first_name'][0] : '';
-	                $last_name = (isset($meta['last_name'][0]) && $meta['last_name'][0] != '') ? $meta['last_name'][0] : '';
-	                $email = $user->user_email;
-	                $role = $user->roles[0];
-                    $registered_date = $user->user_registered;
+				fpassthru( $out );
+			}
+			exit;
+		}
+	}
 
-                    $lineData = array($first_name, $last_name, $email, $role, $registered_date);
-                    fputcsv($out, $lineData, $delimiter);
-                }
+	public function selected_export( $user_ids ) {
+		if ( current_user_can( 'manage_options' ) ) {
+			$delimiter = ",";
+			$filename  = "users-" . date( 'd-m-Y' ) . ".csv";
+			$out       = fopen( 'php://output', 'w' );
+			$fields    = [ 'Имя', 'Фамилия', 'Email', 'Роль', 'Дата регистрации' ];
 
-                header("Content-type: application/force-download");
-                header('Content-Disposition: inline; filename="' . $filename . '";');
+			fputcsv( $out, $fields, $delimiter );
 
-                fpassthru($out);
-            }
-            exit;
-        }
-    }
+			foreach ( $user_ids as $user_id ) {
+				$user_data       = get_userdata( $user_id );
+				$first_name      = ( isset( $user_data->first_name ) && $user_data->first_name != '' ) ? $user_data->first_name : '';
+				$last_name       = ( isset( $user_data->last_name ) && $user_data->last_name != '' ) ? $user_data->last_name : '';
+				$email           = $user_data->user_email;
+				$role            = $user_data->roles[0];
+				$registered_date = $user_data->user_registered;
+				$lineData        = [ $first_name, $last_name, $email, $role, $registered_date ];
 
-    public function selected_export($user_ids)
-    {
-        if (current_user_can('manage_options')) {
-            $delimiter = ",";
-            $filename = "users-" . date('d-m-Y') . ".csv";
+				fputcsv( $out, $lineData, $delimiter );
+			}
 
-            $out = fopen('php://output', 'w');
-            $fields = array('Имя', 'Фамилия', 'Email', 'Роль', 'Дата регистрации');
-            fputcsv($out, $fields, $delimiter);
+			header( "Content-type: application/force-download" );
+			header( 'Content-Disposition: inline; filename="' . $filename . '";' );
 
-            foreach ($user_ids as $user_id) {
-                $user_data = get_userdata($user_id);
-                $role = $user_data->roles[0];
-                $email = $user_data->user_email;
-                $registered_date = $user_data->user_registered;
-
-                $first_name = (isset($user_data->first_name) && $user_data->first_name != '') ? $user_data->first_name : '';
-                $last_name = (isset($user_data->last_name) && $user_data->last_name != '') ? $user_data->last_name : '';
-
-                $lineData = array($first_name, $last_name, $email, $role, $registered_date);
-                fputcsv($out, $lineData, $delimiter);
-            }
-
-            header("Content-type: application/force-download");
-            header('Content-Disposition: inline; filename="' . $filename . '";');
-
-            fpassthru($out);
-        }
-        exit();
-    }
+			fpassthru( $out );
+		}
+		exit();
+	}
 }
 
-if (class_exists('ExportUsers')) {
-    $export_users = new ExportUsers();
-    $export_users->register();
+if ( class_exists( 'ExportUsers' ) ) {
+	$export_users = new ExportUsers();
+	$export_users->register();
 }
 
-register_activation_hook(__FILE__, array($export_users, 'activation'));
-register_deactivation_hook(__FILE__, array($export_users, 'deactivation'));
+register_activation_hook( __FILE__, [ $export_users, 'activation' ] );
+register_deactivation_hook( __FILE__, [ $export_users, 'deactivation' ] );
